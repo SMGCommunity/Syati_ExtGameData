78033a5648d93a907b1e596341867137799fc167
VTXG <VTXG@users.noreply.github.com>
2025-09-03 01:04:00 +0100

    Clean up code

--- source/ExtGameData.cpp
+++ source/ExtGameData.cpp
@@ -1,40 +1,36 @@
 #include "ModuleData_ExtGameData.h"
 #include "ExtGameDataUtil.h"
 
-namespace {
-    void extCreateChunks(ExtGameDataHolder *pHolder) {
-        for (s32 i = 1; i < cExtSaveChunkCount; i++) {
-            BinaryDataChunkBase *pChunk = cExtSaveChunkCreateTable[i]();
+#define BASE_CHUNK_COUNT 6
 
-            if (pChunk) {
-                // TODO Find a better way to do this
-                u32 memberOffset = (u32)pHolder + sizeof(GameDataHolder) + 4 * (i - 1);
-                *(u32 *)memberOffset = (u32)pChunk;
-                OSReport("%p, %d\n", pChunk, sizeof(GameDataHolder) + 4 * (i - 1));
-                pHolder->mBinaryDataChunkHolder->addChunk(pChunk);
-            }
-        }
-
-        pHolder->resetAllData();
-    }
-
-}
-
-kmWrite16(0x804D483A, 6 + cExtSaveChunkCount);
-kmCall(0x804D488C, extCreateChunks);
-
-kmWrite16(0x804DF00E, sizeof(ExtGameDataHolder));
-
-
-void extResetAllData(ExtGameDataHolder* pHolder) {
-    ((BinaryDataChunkBase*)pHolder->mEventValueChecker)->initializeData();
-    BinaryDataChunkBase* pChunk = 0;
-    u32 ptr = 0;
+void extCreateChunks(ExtGameDataHolder *pHolder) {
     for (s32 i = 1; i < cExtSaveChunkCount; i++) {
-        ptr = *((u32*)pHolder+0xB+(i-1));
-        ((BinaryDataChunkBase*)ptr)->initializeData();
+        BinaryDataChunkBase *pChunk = cExtSaveChunkCreateTable[i]();
+
+        if (pChunk) {
+            // TODO Find a better way to do this
+            u32 memberOffset = (u32)pHolder + sizeof(GameDataHolder) + 4 * (i - 1);
+            *(u32 *)memberOffset = (u32)pChunk;
+
+            pHolder->mBinaryDataChunkHolder->addChunk(pChunk);
+        }
+    }
+
+    pHolder->resetAllData();
+}
+
+void extResetAllData(ExtGameDataHolder *pHolder) {
+    ((BinaryDataChunkBase*)pHolder->mEventValueChecker)->initializeData();
+    BinaryDataChunkHolder* pChunkHolder = pHolder->mBinaryDataChunkHolder;
+
+    for (s32 i = BASE_CHUNK_COUNT; i < pChunkHolder->mNumChunks; i++) {
+        pChunkHolder->mChunks[i]->initializeData();
     }
 }
+
+kmWrite16(0x804D483A, BASE_CHUNK_COUNT + cExtSaveChunkCount);
+kmCall(0x804D488C, extCreateChunks);
+kmWrite16(0x804DF00E, sizeof(ExtGameDataHolder));
 
 kmWrite32(0x804D4B6C, 0x7FE3FB78); // mr r3, r31
 kmCall(0x804D4B70, extResetAllData);
