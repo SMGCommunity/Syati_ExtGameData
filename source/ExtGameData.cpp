49a347b6212b702b272a8ee1dafd641c744c4039
Super Hackio <superhaxio@gmail.com>
2026-02-09 14:16:01 -0800

    Now supports all regions of SMG2

--- source/ExtGameData.cpp
+++ source/ExtGameData.cpp
@@ -30,10 +30,17 @@ void extResetAllData(ExtGameDataHolder *pHolder) {
     }
 }
 
-kmWrite16(0x804D483A, BASE_CHUNK_COUNT + cExtSaveChunkCount);
-kmCall(0x804D488C, extCreateChunks);
-kmWrite16(0x804DF00E, sizeof(ExtGameDataHolder));
+#if defined(SB4)
+extern const u8 __ct__14GameDataHolderFv;
+extern const u8 __ct__8UserFileFv;
+extern const u8 resetAllData__14GameDataHolderFv;
 
-kmWrite32(0x804D4B6C, PPC_MR(3, 31)); // mr r3, r31
-kmCall(0x804D4B70, extResetAllData);
-kmWrite32(0x804D4B74, PPC_B(0xC)); // b 0xC
+kmWrite16(&__ct__14GameDataHolderFv + 0xDA, BASE_CHUNK_COUNT + cExtSaveChunkCount);
+kmCall(&__ct__14GameDataHolderFv +0x12C, extCreateChunks);
+
+kmWrite16(&__ct__8UserFileFv +0x2E, sizeof(ExtGameDataHolder));
+
+kmWrite32(&resetAllData__14GameDataHolderFv +0x3C, PPC_MR(3, 31)); // mr r3, r31
+kmCall(&resetAllData__14GameDataHolderFv +0x40, extResetAllData);
+kmWrite32(&resetAllData__14GameDataHolderFv +0x44, PPC_B(0xC)); // b 0xC
+#endif
